ff1820b5232fae03376525cbcd22fa82378f353c
{Poorv} <poorvpatidar2112@gmail.com>
2025-05-04 12:00:36 +0530

    Updated PlayerController Script
    Implemented Sprinting Feature

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -1,16 +1,68 @@
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    PlayerInputs _playerInputs;
+    [SerializeField] Rigidbody playerRB;
+    [SerializeField] float walkSpeed;
+    [SerializeField] float sprintSpeed;
+    private Vector2 _moveInput;
+    private float currentSpeed;
+    private bool _isMovingForward;
+    private bool _isSprinting;
+    private void OnEnable()
     {
+        _playerInputs = new PlayerInputs();
+        _playerInputs.Enable();
+        _playerInputs.Player.Move.performed += ctx => HandleMoveInput(ctx.ReadValue<Vector2>());
+        _playerInputs.Player.Move.canceled += ctx => HandleMoveCancelInput(ctx.ReadValue<Vector2>());
+        _playerInputs.Player.Sprint.performed += ctx => HandleSprintInput();
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _playerInputs.Disable();
+        _playerInputs.Player.Move.performed -= ctx => HandleMoveInput(ctx.ReadValue<Vector2>());
+        _playerInputs.Player.Move.canceled -= ctx => HandleMoveCancelInput(ctx.ReadValue<Vector2>());
+        _playerInputs.Player.Sprint.performed -= ctx => HandleSprintInput();
+
+
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        Vector3 moveVelocity;
+        Vector3 moveDirection = transform.forward * _moveInput.y + transform.right * _moveInput.x;
+        if (_isMovingForward && _isSprinting)
+        {
+            moveVelocity = moveDirection.normalized * sprintSpeed;
+        }
+        else
+        {
+            moveVelocity = moveDirection.normalized * walkSpeed;
+        }  
+        playerRB.linearVelocity = new Vector3(moveVelocity.x, playerRB.linearVelocity.y, moveVelocity.z);
+    }
+    void HandleMoveInput(Vector2 input)
+    {
+        _moveInput = input;
+        _isMovingForward = _moveInput.y > 0.71f;
+    }
+    void HandleMoveCancelInput(Vector2 input)
+    {
+        _moveInput = input;
+        _isMovingForward = false;
+        _isSprinting = false;
+    }
+    void HandleSprintInput()
+    {
+        if (_isMovingForward)
+        {
+            _isSprinting = true;
+        }
     }
 }
